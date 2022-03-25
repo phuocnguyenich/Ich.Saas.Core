297895580407607dd0330cb69c240c198453f95b
Nguyen Ich Phuoc <phuocnguyenich.affm@gmail.com>
2022-03-25 17:49:08 +0700

    feat: auth area

---
+++ Ich.Saas.Core/Ich.Saas.Core/Domain/User.partial.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ich.Saas.Core.Domain
+{
+    public partial class User
+    {
+        #region FullName
+
+        private string fullName;
+
+        [NotMapped]
+        public string FullName
+        {
+            get { return fullName ??= FirstName + "" + LastName; }
+            set => fullName = value;
+        }
+
+        #endregion
+    }
+}
