f67d5e5afead4b8364eaed802d92f8d2facc084b
Nguyen Ich Phuoc <phuocnguyenich.affm@gmail.com>
2022-03-25 17:10:47 +0700

    feat: base module and http extensions

---
+++ Ich.Saas.Core/Ich.Saas.Core/Code/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using Ich.Saas.Core.Code.Extensions;
+using Ich.Saas.Core.Code.Infrastructure;
+using Microsoft.AspNetCore.Http;
+
+namespace Ich.Saas.Core.Code
+{
+    public class BaseModel
+    {
+        private IHttpContextAccessor _httpContextAccessor = ServiceLocator.Resolve<IHttpContextAccessor>();
+
+        public string Referer { get => _httpContextAccessor.PostedReferer() ?? 
+                                       _httpContextAccessor.Referer(); }
+    }
+}
